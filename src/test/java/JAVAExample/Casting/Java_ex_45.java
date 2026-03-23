11fddc0839d6444ae7033c31ed48534f83a4f4cb
abhishek pandey <abhishekpy55@gmail.com>
2026-03-23 12:12:00 +0530

    some code update

---
+++ src/test/java/JAVAExample/Casting/Java_ex_45.java
@@ -0,0 +1,13 @@
+package JAVAExample.Casting;
+
+public class Java_ex_45 {
+    public static void main(String[] args) {
+
+     int a = 10 ;
+        System.out.println(++a); //a=  11
+        System.out.println(++a); // a = 12
+        System.out.println(a++); // a = 12 , them 13
+        System.out.println(a); // a = 13
+    }
+}
+
