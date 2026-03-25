348f2bcecdb6dd63e5bf5c210113d401dad83484
abhishek pandey <abhishekpy55@gmail.com>
2026-03-25 12:00:10 +0530

    code update

--- src/test/java/JAVAExample/StringClass/Java_ex_49.java
+++ src/test/java/JAVAExample/StringClass/Java_ex_49.java
@@ -6,5 +6,13 @@ public class Java_ex_49 {
      String name2=  new String("Abhishek"); // New opretor
 // Both of them actually save the name  name2 in the diffrent way jvm
         // how they store the value in thr jvm
+        System.out.println(Name.length());
+        System.out.println(Name.toLowerCase());
+        // primitive data type :- byte , int, long, short, double, float,
+        // all of them  don't extra functionality
+
+        //  Non-Primitive data type
+      // java is not pure object oriented
+
     }
 }
--- src/test/java/JAVAExample/StringClass/Java_ex_51.java
+++ src/test/java/JAVAExample/StringClass/Java_ex_51.java
@@ -5,6 +5,23 @@ public class Java_ex_51 {
      String Name = "Abhishek";
         System.out.println("Your Name is -->" + Name);
 
+//      java Java is not a pure object-oriented language because it supports primitive data types like int, char,
+//        and boolean, which are not objects. Additionally, Java allows static methods and variables
+//        that can be accessed without creating objects.
 
+//        In Java → primitives exist
+//        In pure OOP → everything should be an object
+//        int a = 10;   // not an objectJava supports primitive types:
+//
+//        int
+//        char
+//        boolean
+//        double, etc.class Demo {
+//            static void show() {
+//                System.out.println("Hello");
+//            }
+//        }
+//
+//        Integer obj = 10;
     }
 }
