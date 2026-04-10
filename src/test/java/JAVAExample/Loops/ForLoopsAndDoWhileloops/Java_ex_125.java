b32019045c6cf6ebb6c3cb175c021ff0fa8dee63
abhishek pandey <abhishekpy55@gmail.com>
2026-04-10 18:00:42 +0530

    loops add exam

---
+++ src/test/java/JAVAExample/Loops/ForLoopsAndDoWhileloops/Java_ex_125.java
@@ -0,0 +1,12 @@
+package JAVAExample.Loops.ForLoopsAndDoWhileloops;
+
+import java.util.Scanner;
+
+public class Java_ex_125 {
+    public static void main(String[] args) {
+  //For loops and While and Do While
+        for (int i = 1 ; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/test/java/JAVAExample/Loops/ForLoopsAndDoWhileloops/Java_ex_126.java
@@ -0,0 +1,13 @@
+package JAVAExample.Loops.ForLoopsAndDoWhileloops;
+
+public class Java_ex_126 {
+    public static void main(String[] args) {
+  //For loops and While and Do While
+
+        int j = 0;
+
+        for(;j<=100;j++){
+            System.out.println(j);
+        }
+    }
+}
